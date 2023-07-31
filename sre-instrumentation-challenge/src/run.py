db3fbd75a72f95aacea964921f3627b744e09aa2
robgutsopedra <roberto.gutierrez@ultimate.ai>
2023-07-31 13:47:35 +0200

    Firs try on github actions

--- sre-instrumentation-challenge/src/run.py
+++ sre-instrumentation-challenge/src/run.py
@@ -5,4 +5,5 @@ from waitress import serve
 
 if __name__ == "__main__":
     port = os.getenv("PORT", default=5000)
+    print("Starting server on port %s", port)
     serve(app, host="0.0.0.0", port=port)
