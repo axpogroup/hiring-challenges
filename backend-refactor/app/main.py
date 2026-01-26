ca812f023a2f891229247d0dc7709ed0d12b3eeb
georkings <georkings88@gmail.com>
2026-01-26 10:21:31 +0100

    - Set reload = settings.debug_mode, in production 'reload' should be False. - Remove startup() call, the '/health' path will be loaded in app.py.

--- backend-refactor/app/main.py
+++ backend-refactor/app/main.py
@@ -1,11 +1,16 @@
 """Main entry point for the application."""
 import uvicorn
-from helpers.startup import startup
-from app import create_app
-
+from app.app import create_app
+from app.core.config import get_settings
 
 app = create_app()
-startup(app)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    settings = get_settings()
+
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=settings.debug_mode 
+    )
